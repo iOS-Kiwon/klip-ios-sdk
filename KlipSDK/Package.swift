8478fde816a971dece69af685db7576f9c15ad00
이기원 <ios.kiwon@gmail.com>
2025-02-04 21:07:07 +0900

    Add Klip Wrappring Package

---
+++ KlipSDK/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KlipSDK",
+    platforms: [.iOS(.v15), .macOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "KlipSDK",
+            targets: ["KlipSDK"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "KlipSDK"
+        ),
+        .binaryTarget(
+            name: "Klip",
+            path: "Frameworks/KlipLib.xcframework"
+        ),
+        .binaryTarget(
+            name: "Klip-Simulator",
+            path: "Frameworks/KlipLib-simulator.xcframework"
+        )
+    ]
+)

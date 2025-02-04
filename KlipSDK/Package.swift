daf0c69d65c616725d19e9d284bbdce13d3b34cc
이기원 <ios.kiwon@gmail.com>
2025-02-04 21:28:17 +0900

    시뮬레이터용 SDK 제거

--- KlipSDK/Package.swift
+++ KlipSDK/Package.swift
@@ -16,15 +16,14 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KlipSDK"
+            name: "KlipSDK",
+            dependencies: [
+                "Klip"
+            ]
         ),
         .binaryTarget(
             name: "Klip",
             path: "Frameworks/KlipLib.xcframework"
-        ),
-        .binaryTarget(
-            name: "Klip-Simulator",
-            path: "Frameworks/KlipLib-simulator.xcframework"
         )
     ]
 )

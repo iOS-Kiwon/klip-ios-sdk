2b0d5292e3ca5b8e302e87fae265aab75f931a10
기원 이 <kiwon.yee@coinone.com>
2025-02-05 10:51:52 +0900

    framework change

---
+++ Frameworks/KlipLib.xcframework/ios-arm64/KlipLib.framework/Headers/klip_sdk.h
@@ -0,0 +1,19 @@
+//
+//  klip_sdk.h
+//  klip-sdk
+//
+//  Created by conan on 2020/09/07.
+//  Copyright © 2020 conan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for klip_sdk.
+FOUNDATION_EXPORT double klip_sdkVersionNumber;
+
+//! Project version string for klip_sdk.
+FOUNDATION_EXPORT const unsigned char klip_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <klip_sdk/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -2,19 +2,18 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
 let package = Package(
     name: "KlipSDK",
     platforms: [.iOS(.v15), .macOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KlipSDK",
-            targets: ["KlipSDK", "Klip"]),
+            targets: ["KlipSDK"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "KlipSDK",
             dependencies: [
@@ -23,7 +22,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Klip",
-            path: "Frameworks/KlipLib.xcframework"
+            path: "Frameworks/KlipLib-simulator.xcframework" // arm64, x86-64를 모두 포함하고 있음.
         )
     ]
 )
